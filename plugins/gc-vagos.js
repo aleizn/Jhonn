bbf1b8bb9a00cab9b1038c1177ce4bd448826511
aleizn <119465229+aleizn@users.noreply.github.com>
2023-08-11 13:58:50 -0500

    Aleizn

--- plugins/gc-vagos.js
+++ plugins/gc-vagos.js
@@ -4,16 +4,16 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴::* ${pesan}`
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
 let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `┣💛 @${mem.id.split('@')[0]}\n`}
+teks += `┣👿 @${mem.id.split('@')[0]}\n`}
 teks += `└𝐉𝐡𝐨𝐧𝐲𝐁𝐨𝐭`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
-handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.help = ['tgg <mesaje>','vagos <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(vagos|vago)$/i
 handler.admin = true
 handler.group = true
 export default handler
